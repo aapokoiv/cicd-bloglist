75f04afadd625507f3160d446e2c1f28b77536b5
Aapo Koivula <koivula.aapo@gmail.com>
2026-08-07 09:42:04 +0300

    Initial commit

---
+++ e2e-testing/tests/blog_app.spec.js
@@ -0,0 +1,119 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith, createBlog } = require('./helper.js')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Aapo Koivula',
+        username: 'aapokoiv',
+        password: 'salainen',
+      },
+    })
+
+    await page.goto('/')
+  })
+
+  describe('Login', () => {
+    test('Login succeeds with the correct username/password ', async ({ page }) => {
+      await expect(page.getByRole('link', { name: 'login' })).toBeVisible()
+      await loginWith(page, 'aapokoiv', 'salainen')
+      await expect(page.getByText('Logged in successfully')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'aapokoiv', 'noooo')
+
+      await expect(page.getByText('wrong username or password')).toBeVisible()
+      await expect(page.getByText('Logged in successfully')).not.toBeVisible()
+    })
+    describe('When logged in', () => {
+      beforeEach(async ({ page }) => {
+        await loginWith(page, 'aapokoiv', 'salainen')
+      })
+
+      test('A logged-in user can create a blog', async ({ page }) => {
+        await page.getByRole('link', { name: 'new blog' }).click()
+        await page.getByLabel('title:').fill('E2E testing')
+        await page.getByLabel('author:').fill('aapo')
+        await page.getByLabel('url:').fill('localhost')
+        await page.getByRole('button', { name: 'create' }).click()
+
+        await expect(page.getByText('E2E testing, -aapo')).toBeVisible()
+      })
+
+      describe('A logged-in user can like blogs', () => {
+        beforeEach(async ({ page }) => {
+          await createBlog(page, 'E2E testing', 'aapo', 'localhost')
+        })
+
+        test('a blog can be liked', async ({ page }) => {
+          await page.getByRole('link', { name: 'E2E testing, -aapo' }).click()
+          await expect(page.getByText('0 Likes')).toBeVisible()
+          await page.getByRole('button', { name: 'like' }).click()
+          await expect(page.getByText('1 Likes')).toBeVisible()
+        })
+
+        test('the creator can delete the blog', async ({ page }) => {
+          await page.getByRole('link', { name: 'E2E testing, -aapo' }).click()
+          page.on('dialog', (dialog) => dialog.accept())
+          await page.getByRole('button', { name: 'delete' }).click()
+        })
+
+        test('another user does not see delete button', async ({ page, request }) => {
+          await request.post('/api/users', {
+            data: {
+              name: 'Not Aapo',
+              username: 'onni',
+              password: 'salainen',
+            },
+          })
+          await page.getByRole('button', { name: 'logout' }).click()
+          await loginWith(page, 'onni', 'salainen')
+
+          await page.getByRole('link', { name: 'E2E testing, -aapo' }).click()
+          await expect(page.getByRole('button', { name: 'delete' })).not.toBeVisible()
+        })
+      })
+
+      describe('When there are multiple blogs with some amount of likes', () => {
+        beforeEach(async ({ page }) => {
+          await createBlog(page, 'Debugging', 'aapo', 'localhost')
+          await createBlog(page, 'Vibin', 'aapo', 'localhost')
+          await createBlog(page, 'E2E testing', 'aapo', 'localhost')
+          await createBlog(page, 'Refactoring', 'aapo', 'localhost')
+
+          const debuggingBlog = page.locator('.blog').filter({ hasText: 'Debugging' })
+          const vibinBlog = page.locator('.blog').filter({ hasText: 'Vibin' })
+          const e2eBlog = page.locator('.blog').filter({ hasText: 'E2E testing' })
+          const refactoringBlog = page.locator('.blog').filter({ hasText: 'Refactoring' })
+
+          await vibinBlog.getByRole('button', { name: 'show' }).click()
+          await vibinBlog.getByRole('button', { name: 'like' }).click()
+          await expect(vibinBlog).toContainText('likes 1')
+          await vibinBlog.getByRole('button', { name: 'like' }).click()
+          await expect(vibinBlog).toContainText('likes 2')
+          await vibinBlog.getByRole('button', { name: 'like' }).click()
+          await expect(vibinBlog).toContainText('likes 3')
+
+          await e2eBlog.getByRole('button', { name: 'show' }).click()
+          await e2eBlog.getByRole('button', { name: 'like' }).click()
+          await expect(e2eBlog).toContainText('likes 1')
+          await e2eBlog.getByRole('button', { name: 'like' }).click()
+          await expect(e2eBlog).toContainText('likes 2')
+
+          await debuggingBlog.getByRole('button', { name: 'show' }).click()
+          await debuggingBlog.getByRole('button', { name: 'like' }).click()
+          await expect(debuggingBlog).toContainText('likes 1')
+        })
+        test('Blogs are sorted correctly by amount of likes', async ({ page }) => {
+          await expect(page.locator('.blog').nth(0)).toContainText('Vibin')
+          await expect(page.locator('.blog').nth(1)).toContainText('E2E testing')
+          await expect(page.locator('.blog').nth(2)).toContainText('Debugging')
+          await expect(page.locator('.blog').nth(3)).toContainText('Refactoring')
+        })
+      })
+    })
+  })
+})
---
+++ frontend/src/components/Blog.test.jsx
@@ -0,0 +1,81 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Blog from './Blog.jsx'
+
+test('all info but no buttons shown for not logged in users', () => {
+  const blog = {
+    id: 'blog-1',
+    title: 'This is a testing blog',
+    author: 'Aapo Koivula',
+    url: 'www.testing.react',
+    likes: 1,
+    user: {
+      id: 'user-1',
+      name: 'Aapo Koivula',
+      username: 'aapo',
+    },
+  }
+
+  render(<Blog blog={blog} />)
+
+  const title = screen.findByText('This is a testing blog')
+  const author = screen.findByText('Aapo Koivula')
+  const url = screen.findByText('www.testing.react')
+  const likes = screen.findByText('likes: 1')
+})
+
+test('like button but no delete button is shown for other users', async () => {
+  const blog = {
+    id: 'blog-1',
+    title: 'This is a testing blog',
+    author: 'Aapo Koivula',
+    url: 'www.testing.react',
+    likes: 1,
+    user: {
+      id: 'user-1',
+      name: 'Aapo Koivula',
+      username: 'aapo',
+    },
+  }
+
+  const loggedInUser = {
+    id: 'user-2',
+    name: 'onni onnekas',
+    username: 'onni',
+  }
+
+  render(<Blog blog={blog} user={loggedInUser} />)
+
+  const user = userEvent.setup()
+  const likeButton = screen.getByText('like')
+})
+
+test('blog creator is shown like and delete button', async () => {
+  const blog = {
+    id: 'blog-1',
+    title: 'This is a testing blog',
+    author: 'Aapo Koivula',
+    url: 'www.testing.react',
+    likes: 1,
+    user: {
+      id: 'user-1',
+      name: 'Aapo Koivula',
+      username: 'aapo',
+    },
+  }
+
+  const loggedInUser = {
+    id: 'user-1',
+    name: 'Aapo Koivula',
+    username: 'aapo',
+  }
+
+  const updateMock = vi.fn()
+
+  render(<Blog blog={blog} user={loggedInUser} />)
+
+  const user = userEvent.setup()
+  const likeButton = screen.getByText('like')
+
+  const deleteButton = screen.getByText('delete')
+})
